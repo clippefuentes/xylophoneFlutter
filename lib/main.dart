359e4c4276e2e8935d4b485d96f06a4831d85e2a
Clyne Fuentes <clippefuentes@gmail.com>
2022-12-30 16:49:41 -0600

    finish exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +10,49 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              PlaySoundButton(color: Colors.red, soundFile: 'note1.wav'),
+              PlaySoundButton(color: Colors.orange, soundFile: 'note2.wav'),
+              PlaySoundButton(color: Colors.yellow, soundFile: 'note3.wav'),
+              PlaySoundButton(color: Colors.green, soundFile: 'note4.wav'),
+              PlaySoundButton(color: Colors.tealAccent , soundFile: 'note5.wav'),
+              PlaySoundButton(color: Colors.blue, soundFile: 'note6.wav'),
+              PlaySoundButton(color: Colors.purple, soundFile: 'note7.wav'),
+            ],
+          )
         ),
       ),
+    );
+  }
+}
+
+class PlaySoundButton extends StatelessWidget {
+  final String soundFile;
+  final Color color;
+
+  const PlaySoundButton({
+    Key key,
+    this.color,
+    this.soundFile
+  }): super(key: key);
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    final player = AudioPlayer();
+    return Expanded(
+      child: TextButton(
+        child: Container(
+          color: color,
+          // width: double.infinity,
+          // height: double.infinity,
+        ),
+        onPressed: () {
+          player.play(AssetSource(soundFile));
+        },
+      )
     );
   }
 }
